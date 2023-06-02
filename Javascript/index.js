1f015883ad52deef9419b776cd291b01517dbad6
manshisharma1372 <manshisharma1372@gmail.com>
2023-06-02 14:31:29 +0530

    part 2

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,3 +1,16 @@
 
 //to print on console window
 console.log('Namaste dunia');
+let a=12;
+console.log(a);
+const num=12;
+// num=13; not allowed
+
+let n=5;
+console.log(n);
+n="mannu"; //can be done
+console.log(n);
+
+for(let i=0;i<=5;i++){
+    console.log(i);
+}
